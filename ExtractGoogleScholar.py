093a1a3a7ebcfda3a15e85a808db81d1cd4221c2
ls <ls87auto@gmail.com>
2013-11-19 23:46:19 +0800

    use the class and add the last step to show the bibTex

--- ExtractGoogleScholar.py
+++ ExtractGoogleScholar.py
@@ -1,15 +1,14 @@
 # Crawl The contents of Google Scholar
 # Author : ls
 # Create : 10/26,2013
-# Revised: 11/16,2013
+# Revised: 11/19,2013
 
 import urllib2
 import re, random
 from bs4 import BeautifulSoup
 
 
-def GoogleScholarTitle(queryTitle):
-    user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0',\
+user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0',\
                'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0',\
                'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533+',\
                '(KHTML, like Gecko) Element Browser 5.0',\
@@ -21,66 +20,54 @@ def GoogleScholarTitle(queryTitle):
                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)', \
                'Chrome/28.0.1468.0 Safari/537.36', \
                'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; TheWorld)']
-    queryTitle = urllib2.quote(queryTitle.replace(' ', '+'))
-##    queryTitle = queryTitle.replace(' ', '+')
-##    print queryTitle
-    url = 'http://scholar.google.com.hk/scholar?hl=zh-CN&q=%s' % queryTitle
-##    print url
-    request = urllib2.Request(url)
-    index = random.randint(0, 9)
-    user_agent = user_agents[index]
-    request.add_header('User-agent', user_agent)
-    response = urllib2.urlopen(request)
-    html = response.read()
-    soup = BeautifulSoup(html)
-    print soup.originalEncoding
-##    print soup
-##    print '---'
-##    rankPaper = soup.findAll('div', attrs={'style':re.compile('z-index:.')}, limit = 1)
-##    print result
-    paperOnClick = soup.findAll('a', attrs = {'class':'gs_nph', 'href':'#'}, limit = 1)
-    mClick = re.search(r'event,\'.+\',', str(paperOnClick[0]))
-    if mClick:
-        print mClick.group()[7:-2]
-        clickProperty = mClick.group()[7:-2]
-        refUrl = 'http://scholar.google.com.hk/scholar?q=info:%s:scholar.google.com/&output=cite&hl=zh-CN' % clickProperty
-        refRequest = urllib2.Request(refUrl)
-        refRequest.add_header('User-agent', user_agent)
-        refResponse = urllib2.urlopen(refRequest)
-        soup = BeautifulSoup(refResponse.read())
-##        bibTex = soup.findAll('a', attrs = {'class':'gs_citi', 'target':'_blank'}, limit = 1)
-        print soup.prettify()
-        
-        
-    else:
-        print 'The step of Click is error' 
-    
-##    if mClick:
-##        strr = 
-    
-##    strr = str(paperOnClick[0])
-##    print strr[1]
-##    m = re.search('event', strr)
-##    if m:
-##        print 1
-##    print m
-##    print m.end()
-##    print strr[m.end():]
-##    m = re.search(r'\'.+\',', strr[m.end():])
-##    print m.group()
-##    strr = paperOnClick[0]
-##    print strr._class_
-##    onClick = paperOnClick[onclick]
-##    print onClick
-##    result = soup.findAll(attrs={'class':re.compile('z-index:%d+')})
-##    result = soup.find('div', attrs)
-    
-    
+class GoogleScholar:
+    def __init__(self):
+        self.index = 0
 
+    def getHtml(self, url):
+        request = urllib2.Request(url)
+        user_agent = user_agents[self.index]
+        request.add_header('User-agent', user_agent)
+        response = urllib2.urlopen(request)
+        return response.read()
+    
+    def searchTitle(self, queryTitle):
+        
+        self.index = random.randint(0, 9)
+        try:
+            # the first layer
+            queryTitle = urllib2.quote(queryTitle.replace(' ', '+'))
+            url = 'http://scholar.google.com.hk/scholar?hl=zh-CN&q=%s' % queryTitle
+            queryRes = self.getHtml(url)
+            soup = BeautifulSoup(queryRes)
+            paperOnClick = soup.findAll('a', attrs = {'class':'gs_nph', 'href':'#'}, limit = 1)
+            mClick = re.search(r'event,\'.+\',', str(paperOnClick[0]))
+            if mClick:
+                # the second layer
+                clickProperty = mClick.group()[7:-2]
+                refUrl = 'http://scholar.google.com.hk/scholar?q=info:%s:scholar.google.com/&output=cite&hl=zh-CN' % clickProperty
+                refRes = self.getHtml(refUrl)
+                soup = BeautifulSoup(refRes)
+                bibTex = soup.find('a', text = re.compile(u'BibTeX'))
+
+                # the third layer
+                bibUrl = 'http://scholar.google.com.hk'+bibTex['href']
+                bibRes = self.getHtml(bibUrl)
+
+                print bibRes
+
+        except:
+            print 'error'
+                
+                
+                
 
 
 title = 'A Coarse-to-fine approach for fast deformable object detection'
-GoogleScholarTitle(title)
+api = GoogleScholar()
+api.searchTitle(title)            
+    
+
 
 
 
